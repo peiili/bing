440182c69df7130ce47c4cefc87bb1b486f09e6a
燕红立 <yanhongli@369exhibition.com>
2022-11-18 21:25:37 +0800

    feat: create blurhash

---
+++ utils/convertblurhash.js
@@ -0,0 +1,51 @@
+const { encode } = require('blurhash');
+const { createCanvas, loadImage, Image } = require('canvas');
+
+function clamp(n) {
+  return Math.min(9, Math.max(1, n));
+}
+
+function doEncode(image, width, height, cb) {
+  const componentX = clamp(4);
+  const componentY = clamp(3);
+  if (image) {
+    const originalCanvas = createCanvas(width, height);
+    const ctx = originalCanvas.getContext('2d');
+    ctx.drawImage(image, 0, 0, originalCanvas.width, originalCanvas.height);
+    setTimeout(() => {
+      const imageData = ctx.getImageData(
+        0,
+        0,
+        width,
+        height,
+      );
+      const blurhash = encode(
+        imageData.data,
+        imageData.width,
+        imageData.height,
+        componentX,
+        componentY,
+      );
+      cb(blurhash);
+    }, 0);
+  }
+}
+
+function imageToHash(src, cb) {
+  const image = new Image();
+  image.onerror = (err) => { throw err; };
+  image.onload = function () {
+    const { width } = image;
+    const { height } = image;
+    loadImage(src).then((img) => {
+      doEncode(img, width, height, (hash) => {
+        cb({ short_hash: hash, short_width: width, short_height: height });
+      });
+    });
+  };
+  image.src = src;
+}
+
+module.exports = {
+  imageToHash,
+};
--- utils/uploadqiniu.js
+++ utils/uploadqiniu.js
@@ -1,7 +1,8 @@
 const qiniu = require('qiniu');
+const { Image, createCanvas } = require('canvas');
 const { uploadImg } = require('./database');
-
-const { accesskey, SecretKey } = require('./../config/bingUrl');
+const { imageToHash } = require('./convertblurhash');
+const { accesskey, SecretKey } = require('../config/bingUrl');
 
 const config = new qiniu.conf.Config();
 config.zone = qiniu.zone.Zone_z0;
@@ -31,12 +32,22 @@ module.exports = {
             throw respErr;
           }
           if (respInfo.statusCode === 200) {
-            const sql = 'insert into `xek_bing` (`id`,`name`,`describe`,`bash`,`create_date`) values (?,?,?,?,NOW());';
-            const value = [new Date().getTime(), respBody.key, JSON.stringify(desc), respBody.hash];
-
-            uploadImg(sql, value, (resp) => {
-              console.log(resp);
-            });
+            const image = new Image();
+            image.onload = function () {
+              const canvas = createCanvas(400, 225);
+              const ctx = canvas.getContext('2d');
+              ctx.drawImage(image, 0, 0);
+              const base = canvas.toDataURL();
+              imageToHash(base, ({ shortHash, shortWidth, shortHeight }) => {
+                const sql = 'insert into `xek_bing` (`id`,`name`,`describe`,`bash`,`short_blurhash`,`short_blurhash_w`,`short_blurhash_h`,`width`,`height`,`create_date`) values (?,?,?,?,?,?,?,?,?,NOW());';
+                const value = [new Date().getTime(), respBody.key, JSON.stringify(desc), respBody.hash, shortHash, shortWidth, shortHeight, image.width, image.height];
+                uploadImg(sql, value, () => {
+                  console.log(`${name}======== done`);
+                });
+              });
+            };
+            image.onerror = (err) => { throw err; };
+            image.src = src;
           } else {
             console.log(respInfo.statusCode);
             console.log(respBody);
